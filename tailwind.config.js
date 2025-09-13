74281aa01059da83bdbab5d4dc2858ce07018395
dallaaaas <dallasaquinods5@gmail.com>
2025-09-13 23:15:22 +0800

    Initial Commit; not yet done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      width: {
+        '128': '32rem',
+        '150': '48rem',
+        '256': '64rem', 
+      },
+      height: {
+        '128': '32rem', 
+        '150': '48rem',
+        '256': '64rem', 
+      },
+    },
+  },
+}
